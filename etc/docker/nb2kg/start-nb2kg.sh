2e4263abb57a59d902f757d16028a65e36e16728
Magyari Sandor Szilard <sancyx@gmail.com>
2019-10-17 15:25:13 +0200

    build images based on banzai spark version

--- etc/docker/nb2kg/start-nb2kg.sh
+++ etc/docker/nb2kg/start-nb2kg.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 export NB_PORT=${NB_PORT:-8888}
-export GATEWAY_HOST=${GATEWAY_HOST:-localhost}
-export KG_URL=${KG_URL:-http://${GATEWAY_HOST}:${NB_PORT}}
+
 export KG_HTTP_USER=${KG_HTTP_USER:-jovyan}
-export KG_REQUEST_TIMEOUT=${KG_REQUEST_TIMEOUT:-30}
+export KG_REQUEST_TIMEOUT=${KG_REQUEST_TIMEOUT:-180}
 export KERNEL_USERNAME=${KG_HTTP_USER}
 
+export KERNEL_LAUNCH_TIMEOUT=360
+
+export KG_URL="http://enterprise-gateway.kubeflow.svc.cluster.local:8888"
 
 echo "Starting nb2kg against gateway: " ${KG_URL}
 echo "Nootbook port: " ${NB_PORT}
@@ -15,4 +17,4 @@ echo "Kernel user: " ${KERNEL_USERNAME}
 echo "${@: -1}"
 
 
-exec /usr/local/bin/start-notebook.sh $*
+exec /usr/local/bin/start-notebook.sh --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.allow_origin='*' --NotebookApp.base_url=${NB_PREFIX} $*
